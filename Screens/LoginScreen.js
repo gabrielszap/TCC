ac0f59ab05c09ea5c7fcbfa6b210253b80d4542c
Gabriel Souza de Paula <gabriel_souza33@hotmail.com>
2019-05-31 20:04:40 -0300

    First commit

---
+++ Screens/LoginScreen.js
@@ -0,0 +1,25 @@
+// import React, { Component } from "react";
+// import { 
+//     View,
+//     Text,
+//     StyleSheet
+// } from "react-native";
+
+// class LoginScreen extends Component {
+//     render() {
+//         return (
+//             <View style={styles.container}>
+//                 <Text>LoginScreen</Text>
+//             </View>
+//         );
+//     }
+// }
+// exportdefault LoginScreen;
+
+// const styles = StyleSheet.create({
+//     container: {
+//         flex: 1,
+//         alignItems: 'center',
+//         justifyContent: 'center'
+//     }
+// });
